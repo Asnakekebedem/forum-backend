720fba0c27031ef7539f167a513489a40cffd9af
Asnakekebedem <asnakemengistukebede@gmail.com>
2023-08-30 14:14:21 -0400

    evangadiforum using react

---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const userRouter = require("./server/api/users/user.router")
+const pool=require('./server/config/database')
+const questionRouter = require("./server/api/question/question.router")
+const answerRouter = require("./server/api/answer/answer.router");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 80;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use("/api/users", userRouter);
+app.use("/api/question", questionRouter);
+app.use("/api/answer", answerRouter);
+
+app.listen(port,"0.0.0.0", () => console.log(`Listening at http://localhost:${port}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
